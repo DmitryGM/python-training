1cbfbeaa4eaad4a9da2b9dd14310ab1a0e5006f2
DmitryGM <erropp@ya.ru>
2018-09-25 19:40:18 +0300

    scaled data

--- ml1.py
+++ ml1.py
@@ -1,8 +1,22 @@
 import csv
 import numpy as np
 import pandas as pd
-
+from sklearn import preprocessing
 from pip.utils import encoding
+
+pd.set_option('display.max_columns', 100)
+pd.set_option('display.max_rows', 100)
+
+
+def try_parse_to_float(string):
+    '''
+    :return: float(string)
+    '''
+    try:
+        x = float(str(string).replace(',', '.'))
+        return x
+    except ValueError:
+        return -1
 
 
 def csv_pandas_reader(filename):
@@ -10,12 +24,62 @@ def csv_pandas_reader(filename):
     Parse a csv file
     """
     df = pd.read_csv(filename, sep=';')
-    pd.set_option('display.max_columns', 100)
-    pd.set_option('display.max_rows', 100)
-    print(df.head(0))
-    print(df.info())
-    print("!!!")
+    # print(df.head(0))
+    # print(df.info())
+    # print(df.describe())
+    # print(df['пол'].value_counts())
+    # print(df.mean())
+    # print("!!!")
     return df
+
+
+def processing_city(column):
+    return column.map(lambda x: 1 if x == 'A' else 0)
+
+
+def processing_months(column):
+    # Родился в мае?
+    return column.map(lambda x: 1 if 5 == x else 0)
+
+
+def processing_floats(column):
+    column = column.map(try_parse_to_float)
+    try:
+        mode = float((column.mode()[0]))
+    except ValueError:
+        mode = 5.0
+
+    return column.map(lambda x: mode if x == -1 else x)
+
+
+def processing_two_numbers(column):
+    pass
+
+
+def processing_beer_classifications(column):
+    return column.map(lambda x: 0 if str(x) == '0' else 1)
+
+
+def df_pandas_processing(df):
+    # print("df_pandas_processing:")
+
+    # df['город рождения'] = df['город рождения'].apply(lambda x: 1 if x == 'A' else 0)
+    # df['город учебы в школе'] = processing_city(df['город учебы в школе'])
+    # df['месяц рождения'] = processing_months(df['месяц рождения'])
+
+    # df['средний школьный балл(математика)'] = processing_floats(df['средний школьный балл(математика)'])
+
+    # df['дорога до вуза(время)']
+    # df['потребление пива'] = processing_beer_classifications(df['потребление пива'])
+
+    # print(df.shape)
+    # print(df.info())
+
+    f1 = processing_floats(df['оценка по математике (школа)'])
+    f2 = processing_floats(df['средний школьный балл(математика)'])
+    f3 = processing_floats(df['рост'])
+    res = processing_floats(df['Y(оценка по математике в первом семестре)'])
+    return pd.concat([f1, f2, f3, res], axis=1)
 
 
 def norm(lst):
@@ -29,58 +93,24 @@ def norm(lst):
     return list(map(lambda x: round((x - minimum) / (maximum - minimum), 1), lst))
 
 
-def processing(data):
-    res = np.array(data)
-
-    height = np.array(norm(res[:, 3]))
-    average_score = np.array(norm(res[:, 7]))
-    math_score = np.array(norm(res[:, 10]))
-
-    res[:, 3] = height
-    res[:, 7] = average_score
-    res[:, 10] = math_score
-
-    res2 = np.concatenate(height[:, np.newaxis])
-    print(res2)
-
-    #print(np.concatenate((height, height, height), axis=0))
-    #year_birth = norm(res[:, 4])
-    #month_birth = norm(res[:, 5])
-    #res[:, 14] = norm(res[:, 14]) # дорога до вуза
-
-    #16 -- ряд в аудитории
-
-    #res[:, 17] = norm(res[:, 17]) # доля пропусков
-
-    # 18 автоматы на экз
-
-    for row in res:
-        if row[1] == "A":
-            row[1] = 1
-        else:
-            row[1] = 0
-
-        if row[2] == "A":
-            row[2] = 1
-        else:
-            row[2] = 0
-
-    rows = np.array(list(range(50))) # !!!
-    columns = [3, 7, 10]
-
-    return res[rows[:, np.newaxis], columns]
-
-
 df = csv_pandas_reader("students.csv")
+df = df_pandas_processing(df)
+print(df)
 
-res = processing(df)
+a = df.values
+print(a)
 
-X = np.array([[1, 4, 11],
-              [5, 2, 23],
-              [12, 11, 3]])
-Xinv = np.linalg.inv(X)
+min_max_scaler = preprocessing.MinMaxScaler()
+x_scaled = min_max_scaler.fit_transform(a)
+print("x_scaled =")
+print(x_scaled)
 
-Xdot = np.dot(X, Xinv)
+
+# print("X =")
+# print(X)
+# Xinv = np.linalg.inv(X)
+#
+# Xdot = np.dot(X, Xinv)
 
 #Xt = np.transpose(X)
 
